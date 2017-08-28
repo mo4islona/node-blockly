c59d7c98864e46e4d38789f974811b438ab1f63c
mo4islona <mo4islona@gmail.com>
2017-08-28 13:01:27 +0300

    Remove lodash dep

--- browser-raw.js
+++ browser-raw.js
@@ -1,14 +1,12 @@
-const _ = require('lodash');
-
 const Blockly = require('./lib/blockly_compressed_browser');
 
 Blockly.setLocale = function(locale) {
-  Blockly.Msg = _.extend(locale, Blockly.Msg);
+  Blockly.Msg = Object.assign(locale, Blockly.Msg);
   Blockly.Msg = Blockly.Msg();
 }
 
 Blockly.setLocale(require('./lib/i18n/en'))
 
-Blockly.Blocks = _.extend(Blockly.Blocks, require('./lib/blocks_compressed_browser')(Blockly));
+Blockly.Blocks = Object.assign(Blockly.Blocks, require('./lib/blocks_compressed_browser')(Blockly));
 
 module.exports = Blockly;
