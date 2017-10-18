51469c78e1887e62470df72ed1ce372f3eb43da7
mo4islona <mo4islona@gmail.com>
2017-10-18 17:35:58 +0700

    Fix broken localization

--- _blockly.js
+++ _blockly.js
@@ -4,10 +4,10 @@ const Blockly = require('./lib/blockly_compressed');
 Blockly.setLocale = function(locale) {
   Blockly.Msg = Object.assign(locale, Blockly.Msg);
   Blockly.Msg = Blockly.Msg();
+}
 
-  Blockly.utils.getMessageArray_ = function () {
-    return Blockly.Msg
-  }
+Blockly.utils.getMessageArray_ = function () {
+  return Blockly.Msg
 }
 
 Blockly.setLocale(require('./lib/i18n/en'))
